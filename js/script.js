26031f5431851bef780ecfc394b44eedc6f68ff0
Davit <davit.janashia1@gmail.com>
2020-07-13 21:54:52 +0200

    mi risponde con ok dopo 5 secondi, e tasto invio funziona (bonus)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 
+  $('input').keyup(function (event){
+    console.log(event.which);
+    if (event.which == 13){
+      // seleziono il .my-green-answer solo quello dentro template e faccio la sua coppia
+      //con click  avevo selezionato concretamente quello che mi serviva e quindi le modifiche avverranno solo su questo elemento speficico
+      var templateGreen = $('.my-template .my-li-green').clone();
+      //valore del input, cioè quello che scrive utente diventa un valore
+      var myInput = $('#input').val();
+      //faccio le modifiche del elemento nipote del clone selezionato.. occhio che io faccio la modifica proprio su un elemento dentro CLONE, e non sul elemento originale
+      templateGreen.find('.my-green-answer').html(myInput);
+      //aggiungo con click dentro ul
+      $('.my-list').append(templateGreen);
+      setTimeout(answers, 5000);
+    }
+
+  });
+
   $('#invia').click(function() {
     // seleziono il .my-green-answer solo quello dentro template e faccio la sua coppia
     //con click  avevo selezionato concretamente quello che mi serviva e quindi le modifiche avverranno solo su questo elemento speficico
@@ -10,7 +27,7 @@ $(document).ready(function(){
     templateGreen.find('.my-green-answer').html(myInput);
     //aggiungo con click dentro ul
     $('.my-list').append(templateGreen);
-    setTimeout(answers, 4000);
+    setTimeout(answers, 5000);
   });
 
   function answers(){
@@ -20,8 +37,6 @@ $(document).ready(function(){
       $('.my-list').append(templateWhite);
 
   }
-
-
 
 }
 );
