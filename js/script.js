0a4433b80f143130503772ecd29a0da678a57af3
Davit <davit.janashia1@gmail.com>
2020-07-14 18:17:23 +0200

    qq

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-$(document).ready(function(){
+// da sistemare il codice come abbiamo fatto stamattina:
+// 1. creare funzione che invia il messaggio
+// 2. chiamare questa funzione dentro il bottone e dentro tasto invio
+// 3. su click anche della risposta deve comparire Delete
+// 4. search
+
+
+
+
 
   $('input').keyup(function (event){
     // console.log(event.which);
-    if (event.which == 13){
+    //valore del input, cioè quello che scrive utente diventa un valore
+    var myInput = $('#input').val();
+    if (event.which == 13 && myInput.length > 0){
       // seleziono il .my-green-answer solo quello dentro template e faccio la sua coppia
       //con click  avevo selezionato concretamente quello che mi serviva e quindi le modifiche avverranno solo su questo elemento speficico
       var templateGreen = $('.my-template .my-li-green').clone();
-      //valore del input, cioè quello che scrive utente diventa un valore
-      var myInput = $('#input').val();
+
       //faccio le modifiche del elemento nipote del clone selezionato.. occhio che io faccio la modifica proprio su un elemento dentro CLONE, e non sul elemento originale
       templateGreen.find('.my-green-answer').html(myInput);
       //aggiungo con click dentro ul
